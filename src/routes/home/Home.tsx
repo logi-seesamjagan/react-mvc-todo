7bddadc4f4e1a43b0af828e8ce78579ff0b79fea
Jagan Langa | சாமி. ஜெகன் லங்கா <jlanga@logitech.com>
2021-08-11 23:50:31 +0530

    Add more info in home

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -3,30 +3,61 @@ import { Link } from "react-router-dom";
 import { IAuthStore } from "../../types";
 import { useAuthService } from "../../utils";
 
+const TechStack = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "ReactJS",
+  "React-Router-Dom",
+  "Typescript",
+];
+if (process.env.REACT_APP_STORE === "redux") {
+  TechStack.push("Redux", "React-Redux");
+} else {
+  TechStack.push("MobX");
+}
+
 export function HomeView({ auth }: { auth: IAuthStore }) {
   return (
     <div className="Page Home">
+      <header>
+        <h2>
+          <span>📒</span>ToDo
+        </h2>
+      </header>
       {auth.status === "logged-in" && (
-        <blockquote>
-          Hello <strong>{auth.user?.userName}</strong>. Please help yourself
-        </blockquote>
+        <section>
+          Hello <strong>{auth.user?.userName}</strong>. Now you can
+          <Link to="/create">Create</Link>or<Link to="/todos">View</Link>your
+          Todos!
+        </section>
       )}
-      <blockquote>
-        This is Home page. anyone can visit this page.
-        <em> No Auth Required!</em>
-      </blockquote>
+      <section>
+        Anyone can visit this page.
+        <cite> No Auth Required!</cite>
+      </section>
+      <section>
+        <header>
+          <h3>Tech Stack</h3>
+        </header>
+        <div className="Techs">
+          {TechStack.map((tech) => (
+            <span key={tech} className="Tech">
+              {tech}
+            </span>
+          ))}
+        </div>
+      </section>
       {auth.status !== "logged-in" && (
         <>
-          <blockquote>
-            To visit other pages, you need to{" "}
+          <section>
+            To access other pages, you need to
             <Link className="LoginLink" to="/login">
-              Login
+              Login.
             </Link>
-          </blockquote>
-          <blockquote>
-            Didn't have an account with us? <Link to="/register">Create</Link>{" "}
-            one!
-          </blockquote>
+            If you don't have an account? Then
+            <Link to="/register">Register</Link>one!
+          </section>
         </>
       )}
     </div>
